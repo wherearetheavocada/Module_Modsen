c2ff8ecd161fbf691781bfaf529df255022a4ebf
Your Name <you@example.com>
2024-07-01 03:42:50 +0300

    add module2

---
+++ JS-Practice-React/task2/src/App.js
@@ -0,0 +1,12 @@
+import TextComponent from './components/TextComponent';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <TextComponent text={"Предложение 1. Предложение 2. Предложение 3. Предложение 4."}/>
+    </div>
+  );
+}
+
+export default App;
